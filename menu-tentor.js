bcb9be38d5225614b285c1917d5f7e31854af52d
manderacorp <brandy.brandy57@gmail.com>
2026-06-08 13:06:23 +0700

    Add tentor management functionality

---
+++ menu-tentor.js
@@ -0,0 +1,20 @@
+// Struktur kolom tetap: ID, Nama Tentor, Keahlian, No HP, Username, Password
+const HEADERS_TENTOR = ["ID", "Nama Tentor", "Keahlian", "No HP", "Username", "Password"];
+
+async function fetchTentor() {
+    currentActiveMenu = "tentor";
+    const container = document.getElementById('container-tentor');
+    container.innerHTML = `<span class="text-xs text-slate-400"><i class="fa-solid fa-spinner animate-spin mr-1"></i> Memuat Data Tentor...</span>`;
+
+    try {
+        const response = await fetch(`${API_URL}?action=getDataTentor`);
+        const res = await response.json();
+        renderTableModular(container, res, HEADERS_TENTOR, 'Data Tentor');
+    } catch (err) {
+        container.innerHTML = `<div class="text-xs text-rose-500 py-4 text-center">Gagal memuat database tentor.</div>`;
+    }
+}
+
+window.openCreateTentor = function() {
+    setupModalDinamis("Tambah Tentor Baru", "Data Tentor", "create", HEADERS_TENTOR);
+};
