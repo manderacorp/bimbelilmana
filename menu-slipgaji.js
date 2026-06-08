1d10457f3984d3a5a4ed989dc6e824e5b25f202d
manderacorp <brandy.brandy57@gmail.com>
2026-06-08 13:07:26 +0700

    Add slipgaji.js for managing salary slips

---
+++ menu-slipgaji.js
@@ -0,0 +1,20 @@
+// Struktur kolom tetap: ID Slip, Bulan, Nama Tentor, Gaji Pokok, Bonus, Total Diterima
+const HEADERS_SLIPGAJI = ["ID Slip", "Bulan", "Nama Tentor", "Gaji Pokok", "Bonus", "Total Diterima"];
+
+async function fetchSlipgaji() {
+    currentActiveMenu = "slipgaji";
+    const container = document.getElementById('container-slipgaji');
+    container.innerHTML = `<span class="text-xs text-slate-400"><i class="fa-solid fa-spinner animate-spin mr-1"></i> Memuat Slip Gaji...</span>`;
+
+    try {
+        const response = await fetch(`${API_URL}?action=getSlipgaji`);
+        const res = await response.json();
+        renderTableModular(container, res, HEADERS_SLIPGAJI, 'Slip Gaji');
+    } catch (err) {
+        container.innerHTML = `<div class="text-xs text-rose-500 py-4 text-center">Gagal memuat log penggajian.</div>`;
+    }
+}
+
+window.openCreateSlipgaji = function() {
+    setupModalDinamis("Buat Slip Gaji Tentor", "Slip Gaji", "create", HEADERS_SLIPGAJI);
+};
